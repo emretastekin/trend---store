db559a5cc3321584c141d7e5290170afc46087c8
Emre Taştekin <emretastekin2369@gmail.com>
2025-10-12 12:56:00 +0300

    tüm componentler tamamlandı

--- trend-store/src/App.jsx
+++ trend-store/src/App.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
+import Cart from "./components/Cart/Cart"
 import Hero from "./components/Hero/Hero"
 import Header from "./components/Layout/Header"
 import Products from "./components/Products/Products"
 
 function App() {
 
+  const [cartIsShow, setCartIsshow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsshow(true)
+  }
+
+  const hideCartHandler = (e) => {
+    e.preventDefault();
+    setCartIsshow(false)
+  }
+
   return (
     <div className="App">
-      <Header />
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}/>
       <Hero />
       <Products />
     </div>
---
+++ trend-store/src/components/Cart/Cart.jsx
@@ -0,0 +1,36 @@
+import "./Cart.css";
+import CartItem from "./CartItem";
+import products from "../../productData";
+import Offcanvas from "../UI/Offcanvas";
+
+const Cart = (props) => {
+
+    const cartItems = (
+        <ul className="cart-items">
+            {products.map((product) => (
+                <CartItem key={product.id} product={product}/>
+            ))}
+        </ul>
+    );
+
+    return (
+        <Offcanvas onClose={props.onClose}> 
+                <div className="cart-head">
+                    <h2>Sepetim</h2>
+                    <a href="/" className="cart-close" onClick={props.onClose}>X</a>
+                </div>
+                {cartItems}
+                <div className="total">
+                    <span>Toplam Değer</span>
+                    <span>10 ₺</span>
+                </div>
+                <div className="actions">
+                    <button className="cart-order">Sipariş Ver</button>
+                    <button className="cart-clear">Temizle</button>
+                </div>
+        </Offcanvas>
+    );
+};
+
+export default Cart;
+
