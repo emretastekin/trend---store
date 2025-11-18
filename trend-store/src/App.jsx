85cdfcea41ec74a570aad8cb2cfe2853184d8b60
Emre Taştekin <emretastekin2369@gmail.com>
2025-11-18 10:06:23 +0300

    react uygulamalarında veri tabanı bağlantısı ve http istekleri konusu tamamlandı

--- trend-store/src/App.jsx
+++ trend-store/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero"
 import Header from "./components/Layout/Header"
 import Products from "./components/Products/Products"
 import CartProvider from "./context/CartProvider"
+import FormInputs from "./components/Form/FormInputs"
 
 function App() {
 
